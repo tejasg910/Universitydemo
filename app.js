0a310f245aa2753095b09f472db3cf03d2eae16d
Tejas Giri <tejasgiri910@gmail.com>
2022-10-30 10:24:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path')
+const app   = express();
+const port  =  3000;
+app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', "hbs");
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.listen(port, ()=>{
+    console.log("server is running at ", port)
+})
